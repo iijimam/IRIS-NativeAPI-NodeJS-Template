0e36193668f4d6812e3c4d99037a953c59cb2ec4
iijimam <miijima@intersystems.com>
2021-05-24 13:40:35 +0000

    irisnativeモジュールインストール修正

--- Node.js/npminstall.bat
+++ Node.js/npminstall.bat
@@ -1,7 +1,6 @@
 @echo off
 git clone https://github.com/Intersystems-jp/IRISModules.git
 copy %CD%\src\package.json %CD%\
-xcopy /E /Y IRISModules\node\intersystems-iris-native %CD%\intersystems-iris-native\
-call npm install --save intersystems-iris-native
+call npm install --save IRISModules\node\intersystems-iris-native
 
 call npm install express --save
--- Node.js/npminstall.sh
+++ Node.js/npminstall.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
+rm -rf IRISModules
 # irisnativeモジュールインストールのため、git clone
 git clone https://github.com/Intersystems-jp/IRISModules.git
-cp -r IRISModules/node/intersystems-iris-native .
-cp ./src/package.json .
-npm install --save intersystems-iris-native
+npm install --save IRISModules/node/intersystems-iris-native
 
 # サンプルで使用するモジュールをインストール
 npm install express --save
