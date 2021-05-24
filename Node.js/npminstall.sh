da6e6730d05b9f755a8e124000e9afce717a212e
iijimam <miijima@intersystems.com>
2021-05-24 03:42:20 +0000

    順番変更

--- Node.js/npminstall.sh
+++ Node.js/npminstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# サンプルで使用するモジュールをインストール
-npm install express --save
-
 # irisnativeモジュールインストールのため、git clone
 git clone https://github.com/intersystems/quickstarts-nodejs.git
 cp -r quickstarts-nodejs/Solutions/intersystems-iris-native .
 cp ./src/package.json .
 npm install --save intersystems-iris-native
+
+# サンプルで使用するモジュールをインストール
+npm install express --save
