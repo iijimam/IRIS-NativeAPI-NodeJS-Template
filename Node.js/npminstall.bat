8f488944acfc70aec44cdeef8b2ffd3bd1afa527
iijimam <miijima@intersystems.com>
2021-05-24 14:01:12 +0000

    インストールモジュール名（name）の変更

--- Node.js/npminstall.bat
+++ Node.js/npminstall.bat
@@ -1,7 +1,6 @@
 @echo off
 rd /s /q IRISModules
 git clone https://github.com/Intersystems-jp/IRISModules.git
-copy IRISModules\node\intersystems-iris-native\package.json %CD%\
 call npm install --save IRISModules\node\intersystems-iris-native
 
 call npm install express --save
